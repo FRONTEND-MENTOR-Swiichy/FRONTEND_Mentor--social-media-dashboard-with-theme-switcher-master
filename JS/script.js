64badc8237c3ee4f7497cc312d7cb3120e8bf7c9
Jansana Dylan <BOOMHEADSHOTMn@gmail.com>
2020-10-19 15:27:20 +0200

    Finaly

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,6 @@
-const checkbox = document.getElementById('checkbox');
 
+const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', () => {
-    
     document.body.classList.toggle('dark');
-    
 });
+
